d2b48f6241c194c326cdefdc9ef39eed4b5471b5
Tarak Blah <halbkarat@gmail.com>
2017-03-27 23:19:27 +0200

    Fixed typo.

--- user_profiles/admin.py
+++ user_profiles/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from user_profile.models import UserProfile
+from user_profiles.models import UserProfile
 
 
 class UserProfileInline(admin.StackedInline):
